760c79c02db9c5dd6d34b5a47fae6bcea4c1139f
mradepah <34737782+mradepah@users.noreply.github.com>
2019-10-24 05:15:15 +0000

    Finalised grading logic and added visuals to correspond to right or wrong answers

--- app/src/main/java/com/example/myquizzapp/MainActivity.java
+++ app/src/main/java/com/example/myquizzapp/MainActivity.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,6 +14,7 @@ import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
 
     int score = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,32 +42,70 @@ public class MainActivity extends AppCompatActivity {
         RadioButton question2radio1 = findViewById(R.id.question2radio1);
         boolean question2radio1_ischecked = question2radio1.isChecked();
         RadioButton question2radio2 = findViewById(R.id.question2radio2);
-        boolean question2radio2_ischecked = question2radio1.isChecked();
+        boolean question2radio2_ischecked = question2radio2.isChecked();
 
 //    checks for user entered variable in question 3
-
         EditText question3answer = findViewById(R.id.question3answer);
         Editable answer3editable = question3answer.getText();
         String answer3 = answer3editable.toString();
+        TextView question3Option1 = findViewById(R.id.question3Option1);
+        TextView question3Option2 = findViewById(R.id.question3Option2);
+        TextView question3Option3 = findViewById(R.id.question3Option3);
+        TextView question3Option4 = findViewById(R.id.question3Option4);
 
 //    checks for radio check in question 4
-
         RadioButton question4radio1 = findViewById(R.id.question4radio1);
         boolean question4radio1_ischecked = question4radio1.isChecked();
         RadioButton question4radio2 = findViewById(R.id.question4radio2);
         boolean question4radio2_ischecked = question4radio2.isChecked();
 
 //    predetermined answers for each question
-//    answers for question 1
-        if (question1ChkBx1_ischecked && question1ChkBx2_ischecked && question1ChkBx4_ischecked) {
-            score += 20;
-        } else if (question2radio1_ischecked && question2radio2_ischecked) {
-            score += 20;
-        } else if (answer3.equals("A")) {
-            score += 20;
-        } else if (question4radio1_ischecked) {
+//   correct answers change color to green, wrong answers change color to red
+        if (question1ChkBx1_ischecked) {
+            question1ChkBx1.setTextColor(getResources().getColor(R.color.colorGreen));
+            score += 10;
+        }
+        if (question1ChkBx2_ischecked) {
+            question1ChkBx2.setTextColor(getResources().getColor(R.color.colorGreen));
+            score += 10;
+        }
+        if (question1ChkBx3_ischecked) {
+            question1ChkBx3.setTextColor(getResources().getColor(R.color.colorRed));
+        }
+        if (question1ChkBx4_ischecked) {
+            question1ChkBx4.setTextColor(getResources().getColor(R.color.colorRed));
+        }
+        if (question2radio1_ischecked) {
+            question2radio1.setTextColor(getResources().getColor(R.color.colorGreen));
             score += 20;
         }
+        if (question2radio2_ischecked) {
+            question2radio2.setTextColor(getResources().getColor(R.color.colorRed));
+        }
+        if (answer3.equals("A") || answer3.equals("a")) {
+            question3Option1.setTextColor(getResources().getColor(R.color.colorGreen));
+            score += 20;
+        }
+        if (answer3.equals("B") || answer3.equals("b")) {
+            question3Option2.setTextColor(getResources().getColor(R.color.colorRed));
+
+        }
+        if (answer3.equals("C") || answer3.equals("c")) {
+            question3Option3.setTextColor(getResources().getColor(R.color.colorRed));
+
+        }
+        if (answer3.equals("D") || answer3.equals("d")) {
+            question3Option4.setTextColor(getResources().getColor(R.color.colorRed));
+
+        }
+        if (question4radio1_ischecked) {
+            question4radio1.setTextColor(getResources().getColor(R.color.colorGreen));
+            score += 20;
+        }
+        if (question4radio2_ischecked) {
+            question4radio2.setTextColor(getResources().getColor(R.color.colorRed));
+        }
+//      displays username and score in toast message
         Toast.makeText(getApplicationContext(), String.format(Username + " your score is  %s ", score), Toast.LENGTH_LONG).show();
     }
 
